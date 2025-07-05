65ef563b0977a522fa9fff6c9b7fd76127e914f2
kazari4 <skidyuk86115@gmail.com>
2025-07-05 16:26:32 +0900

    サーバーサイドプログラムの作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+// src/App.jsx
+import { useEffect, useState } from "react";
+
 function App() {
+  const [message, setMessage] = useState("Loading...");
+
+  useEffect(() => {
+    fetch("/.netlify/functions/getData")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch(() => setMessage("Error fetching data"));
+  }, []);
+
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <h1>Message from Server:</h1>
+      <p>{message}</p>
     </div>
   );
 }
