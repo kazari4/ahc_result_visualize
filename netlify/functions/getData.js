a77ff4fef6323b629667ecdf45987f7fd5861383
kazari4 <skidyuk86115@gmail.com>
2025-07-06 16:16:04 +0900

    results standingsのデータをfetchしてそれを出力するよう変更

--- netlify/functions/getData.js
+++ netlify/functions/getData.js
@@ -2,28 +2,47 @@ exports.handler = async function (event, context) {
   const session = process.env.REVEL_SESSION;
   const contestName = 'ahc049';
 
-  const url = `https://atcoder.jp/contests/${contestName}/results/json`;
+  const urls = [
+    `https://atcoder.jp/contests/${contestName}/results/json`,
+    `https://atcoder.jp/contests/${contestName}/standings/json`
+  ];
+
+  const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
   try {
-    const res = await fetch(url, {
-      headers: {
-        'Cookie': `REVEL_SESSION=${session}`,
+    const results = [];
+
+    for (let i = 0; i < urls.length; i++) {
+      const res = await fetch(urls[i], {
+        headers: {
+          'Cookie': `REVEL_SESSION=${session}`,
+        }
+      });
+
+      if (!res.ok) {
+        return {
+          statusCode: res.status,
+          body: JSON.stringify({ error: `AtCoder responded with ${res.status}` })
+        };
       }
-    });
 
-    if (!res.ok) {
-      return {
-        statusCode: res.status,
-        body: JSON.stringify({ error: `AtCoder responded with ${res.status}` })
-      };
+      const json = await res.json();
+      results.push(json);
+
+      // 1秒待つ
+      if (i < urls.length - 1) {
+        await delay(1000);
+      }
     }
-
-    const json = await res.json();
 
     return {
       statusCode: 200,
-      body: JSON.stringify(json),
+      body: JSON.stringify({
+        resultsJson: results[0],
+        standingsJson: results[1],
+      }),
     };
+
   } catch (err) {
     return {
       statusCode: 500,
--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
 import { useEffect, useState } from "react";
 
 function App() {
+  const [resultData, setResultData] = useState(null);
+  const [standingData, setStandingData] = useState(null);
+
   useEffect(() => {
     fetch('/.netlify/functions/getData')
       .then(res => res.json())
-      .then(data => console.log("取得成功:", data))
+      .then(data => {
+        setResultData(data.resultsJson);
+        setStandingData(data.standingsJson);
+      })
       .catch(err => console.error("取得失敗:", err));
   }, []);
 
-  return (
-    <div>
-      <h1>Hello!!</h1>
-    </div>
-  );
+  if (resultData === null) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  } else {
+    console.log(resultData)
+    console.log(standingData)
+    return (
+      <div>
+        <h1>Data Fetched!</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
