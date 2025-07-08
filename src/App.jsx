876311bb60e5d816e83e0cecff25bf568e518a34
kazari4 <skidyuk86115@gmail.com>
2025-07-08 11:59:23 +0900

    メインの処理をMain.jsxに移植

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,13 @@
-import { useEffect, useState } from "react";
-import { convertData } from "./convertData";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
-  const [resultData, setResultData] = useState(null);
-  const [standingData, setStandingData] = useState(null);
-
-  useEffect(() => {
-    fetch('/.netlify/functions/getData')
-      .then(res => res.json())
-      .then(data => {
-        setResultData(data.resultsJson);
-        setStandingData(data.standingsJson);
-      })
-      .catch(err => console.error("取得失敗:", err));
-  }, []);
-
-  if (resultData === null) {
-    return (
-      <div>
-        <h1>Loading...</h1>
-      </div>
-    );
-  } else {
-    const new_data = convertData(resultData, standingData);
-    console.log(new_data);
-    return (
-      <div>
-        <h1>Data Fetched!</h1>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <Header />
+      <Main />
+    </div>
+  );
 }
 
 export default App;
