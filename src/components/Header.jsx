a2c021d8804dac89cd0f082bfeb84143e7d733cc
kazari4 <skidyuk86115@gmail.com>
2025-07-08 11:21:45 +0900

    headerを作成

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header className="hero is-info is-small">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title has-text-weight-bold">Scatter Plot of Iris Data</h1>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
