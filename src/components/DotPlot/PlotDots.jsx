c6187079ac44b2ff3ab61ad6b817fe5ec3ffe0fe
kazari4 <skidyuk86115@gmail.com>
2025-08-02 03:40:15 +0900

    ハイライトユーザー処理を隔離

--- src/components/DotPlot/PlotDots.jsx
+++ src/components/DotPlot/PlotDots.jsx
@@ -1,28 +1,19 @@
-import { createScale } from "../../utils/createScale";
-
-function PlotDots({ data, highlightUser, x, height }) {
-  const highlight = data.find(d => d.UserScreenName === highlightUser)
-
-  const yRangeMax = height - 20;
-
-  const yRange = [yRangeMax, 0]
-
-  const yScale = createScale(data, "Score", yRange)
-
+function PlotDots({ data, contestName, x, yScale, height }) {
   const dots = data.map(({ Score, UserScreenName }) => (
     <circle key={UserScreenName} cx={x} cy={yScale(Score)} r="3" fill="black" style={{ opacity: 0.05 }} />
   ))
 
-  if (highlight != null) {
-    dots.push(
-      <circle
-        key="circle-highlight"
-        cx={x}
-        cy={yScale(highlight.Score)}
-        r={3}
-        fill="red"
-      />)
-  }
+  dots.push(
+    <text
+      key={contestName}
+      x={x}
+      y={height}
+      fontSize={14}
+      textAnchor="middle"
+    >
+      {contestName}
+    </text>
+  )
 
   return dots;
 
