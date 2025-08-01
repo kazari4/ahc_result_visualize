a7f0db677c3df6147b75893970c52e5144ec0f28
kazari4 <skidyuk86115@gmail.com>
2025-08-01 18:00:52 +0900

    svgのサイズを可変に変更

--- src/components/DotPlot/DotPlot.jsx
+++ src/components/DotPlot/DotPlot.jsx
@@ -1,10 +1,10 @@
 import PlotDots from "./PlotDots";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import * as d3 from "d3";
 
 function DotPlot({ highlightUser }) {
-  const width = 1000;
-  const height = 400;
+  const containerRef = useRef(null);
+  const [dimensions, setDimensions] = useState({ width: 1000, height: 400 });
 
   const [allData, setAllData] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
@@ -13,7 +13,22 @@ function DotPlot({ highlightUser }) {
     `ahc${String(i + 1).padStart(3, "0")}`
   );
 
-  // コンテストデータを取得、最初のみ
+  useEffect(() => {
+    const observer = new ResizeObserver(([entry]) => {
+      const { width, height } = entry.contentRect;
+      setDimensions({ width, height });
+    });
+
+    if (containerRef.current) {
+      observer.observe(containerRef.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  // データ取得
   useEffect(() => {
     Promise.all(
       contests.map(name =>
@@ -26,8 +41,7 @@ function DotPlot({ highlightUser }) {
     });
   }, []);
 
-
-  // highlightUser が含まれるデータのみフィルタリング
+  // フィルタリング
   useEffect(() => {
     const filtered = allData.filter(({ data }) =>
       data.some(entry => entry.UserScreenName === highlightUser)
@@ -35,21 +49,29 @@ function DotPlot({ highlightUser }) {
     setFilteredData(filtered);
   }, [highlightUser, allData]);
 
-  const xRangeMax = width - 100;
-  const xRange = [0, xRangeMax];
-  const xScale = d3.scaleLinear().domain([0, filteredData.length]).range(xRange);
+  const xRangeMax = dimensions.width - 100;
+  const xScale = d3
+    .scaleLinear()
+    .domain([0, filteredData.length])
+    .range([0, xRangeMax]);
 
   const dotPlotArr = filteredData.map((contestData, i) => (
-    <PlotDots key={contestData.name} data={contestData.data} highlightUser={highlightUser} x={xScale(i)} height={height} />
-  ))
+    <PlotDots
+      key={contestData.name}
+      data={contestData.data}
+      highlightUser={highlightUser}
+      x={xScale(i)}
+      height={dimensions.height}
+    />
+  ));
 
   return (
-    <svg width={width} height={height}>
-      <g transform="translate(50,10)">
-        {dotPlotArr}
-      </g>
-    </svg>
-  )
+    <div ref={containerRef} style={{ width: "100%", height: "400px" }}>
+      <svg width={dimensions.width} height={dimensions.height}>
+        <g transform="translate(50,10)">{dotPlotArr}</g>
+      </svg>
+    </div>
+  );
 }
 
 export default DotPlot;
