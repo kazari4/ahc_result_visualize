df6634455a4f9d635c62ea5aa3f29a55ba123475
kazari4 <skidyuk86115@gmail.com>
2025-08-02 05:40:18 +0900

    サイズの調整

--- src/components/LineChart/LineChart.jsx
+++ src/components/LineChart/LineChart.jsx
@@ -4,28 +4,40 @@ import PlotXAxis from "./PlotXAxis";
 import PlotYAxis from "./PlotYAxis";
 import PlotPerfColor from "./PlotPerfColor";
 
-function LineChart({ data, highlightUser }) {
-  const width = 900;
-  const height = 400;
+function LineChart({ allData, selectedContest, highlightUser }) {
+  if (selectedContest === null) {
+    return (
+      <h1>コンテストが選択されていません</h1>
+    )
+  }
+
+  const data = allData.find(d => d.name === selectedContest).data
+  console.log(data)
+
+  const width = 1500;
+  const height = 600;
 
   const xRangeMax = width - 200;
   const yRangeMax = height - 100;
 
-  const xRange = [0, xRangeMax]
+  const xRange = [10, xRangeMax]
   const yRange = [yRangeMax, 0]
 
-  const xScale = createScale(data, "Rank", xRange)
+  const xScale = createScale(data, "Rank", xRange).nice()
   const yScale = createScale(data, "Score", yRange)
 
   return (
-    <svg width={width} height={height}>
-      <g transform="translate(150,20)">
-        <PlotPerfColor data={data} yScale={yScale} xRangeMax={xRangeMax} yRangeMax={yRangeMax} />
-        <PlotLine data={data} xScale={xScale} yScale={yScale} xRangeMax={xRangeMax} yRangeMax={yRangeMax} highlightUser={highlightUser} />
-        <PlotXAxis scale={xScale} xRangeMax={xRangeMax} yRangeMax={yRangeMax} />
-        <PlotYAxis scale={yScale} xRangeMax={xRangeMax} yRangeMax={yRangeMax} />
-      </g>
-    </svg>
+    <div>
+      <h1>選択しているコンテスト：{selectedContest}</h1>
+      <svg width={width} height={height}>
+        <g transform="translate(150,20)">
+          <PlotPerfColor data={data} yScale={yScale} xRangeMax={xRangeMax} yRangeMax={yRangeMax} />
+          <PlotLine data={data} xScale={xScale} yScale={yScale} xRangeMax={xRangeMax} yRangeMax={yRangeMax} highlightUser={highlightUser} />
+          <PlotXAxis scale={xScale} xRangeMax={xRangeMax} yRangeMax={yRangeMax} />
+          <PlotYAxis scale={yScale} xRangeMax={xRangeMax} yRangeMax={yRangeMax} />
+        </g>
+      </svg>
+    </div>
   )
 }
 export default LineChart;
