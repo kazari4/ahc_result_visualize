39b1eafdece450864ec2a312c0acd40d84e598b6
kazari4 <skidyuk86115@gmail.com>
2025-08-02 06:20:48 +0900

    線を削除

--- src/components/LineChart/PlotLine.jsx
+++ src/components/LineChart/PlotLine.jsx
@@ -4,22 +4,6 @@ function PlotLine({ data, xScale, yScale, xRangeMax, yRangeMax, highlightUser })
 
   const highlight = data.find(d => d.UserScreenName === highlightUser)
 
-  for (let i = 0; i < data.length - 1; i++) {
-    const p1 = data[i]
-    const p2 = data[i + 1]
-
-    lines.push(
-      <line
-        key={`line${i}`}
-        x1={xScale(p1.RankIndex)}
-        y1={yScale(p1.Score)}
-        x2={xScale(p2.RankIndex)}
-        y2={yScale(p2.Score)}
-        stroke="black"
-      />
-    )
-  }
-
   for (let i = 0; i < data.length; i++) {
     const p = data[i]
     circles.push(
