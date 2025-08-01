7e3c5838abc43d63df700d450bf22052fc598696
kazari4 <skidyuk86115@gmail.com>
2025-08-01 18:00:30 +0900

    apiアクセス部分を削除

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,46 +1,21 @@
 import { useEffect, useState } from "react";
-import { convertData } from "../utils/convertData";
 import { createScale } from "../utils/createScale";
-import ContestSelector from "./ContestSelector/ContestSelector";
 import UserNameInput from "./UserNameInput/UserNameInput";
 import LineChart from "./LineChart/LineChart";
 import DotPlot from "./DotPlot/DotPlot";
 
 function Main() {
-  const [resultData, setResultData] = useState(null);
-  const [standingData, setStandingData] = useState(null);
-  const [contestName, setContestName] = useState("ahc050");
+  const [selectedContest, setSelectedContest] = useState("ahc050");
 
   const [highlightUser, setHighlightUser] = useState(null);
-
-  useEffect(() => {
-    fetch(`/.netlify/functions/getData?contestName=${contestName}`)
-      .then(res => res.json())
-      .then(data => {
-        setResultData(data.resultsJson);
-        setStandingData(data.standingsJson);
-      })
-      .catch(err => console.error("取得失敗:", err));
-  }, [contestName]);
-
-  if (resultData === null || standingData === null) {
-    return (
-      <div>
-        <h1>Loading...</h1>
-      </div>
-    );
-  } else {
-    const convertedData = convertData(resultData, standingData);
-    return (
-      <div className="section">
-        <h1>Choose Contest</h1>
-        <ContestSelector selectedContest={contestName} onChange={setContestName} />
-        <UserNameInput onChange={setHighlightUser} />
-        <LineChart data={convertedData} highlightUser={highlightUser} createScale={createScale} />
-        <DotPlot highlightUser={highlightUser} />
-      </div>
-    )
-  }
+  return (
+    <div className="section">
+      <h1>Choose Contest</h1>
+      <UserNameInput onChange={setHighlightUser} />
+      <DotPlot highlightUser={highlightUser} />
+    </div>
+  )
 }
+
 
 export default Main;
