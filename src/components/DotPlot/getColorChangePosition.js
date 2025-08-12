94796a4f64e386d1f524dc0260e6dc996b6f2103
kazari4 <skidyuk86115@gmail.com>
2025-08-12 13:08:02 +0900

    performanceの色が変わる座標を取得する関数の作成

---
+++ src/components/DotPlot/getColorChangePosition.js
@@ -0,0 +1,35 @@
+function getColor(perf) {
+  if (perf >= 2800) {
+    return "rgb(255,178,178)"
+  } else if (perf >= 2400) {
+    return "rgb(255,216,178)"
+  } else if (perf >= 2000) {
+    return "rgb(236,236,178)"
+  } else if (perf >= 1600) {
+    return "rgb(178,178,255)"
+  } else if (perf >= 1200) {
+    return "rgb(178,236,236)"
+  } else if (perf >= 800) {
+    return "rgb(178,216,178)"
+  } else if (perf >= 400) {
+    return "rgb(216,197,178)"
+  } else {
+    return "rgb(216,216,216)"
+  }
+}
+
+
+export function getColorChangePosition(contestData, yScale) {
+  const colorChange = []
+  for (let i = 0; i < contestData.length - 1; i++) {
+    const curPerfColor = getColor(contestData[i].Performance)
+    const nextPerfColor = getColor(contestData[i + 1].Performance)
+    if (curPerfColor != nextPerfColor) {
+      const yPosition = (yScale(data[i].Score) + yScale(data[i + 1].Score)) / 2
+      colorChange.push({
+        curPerfColor: yPosition
+      })
+    }
+  }
+}
+
