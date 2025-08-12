8e8dbb30e63d141419d4b7e32d2a5223f446d5db
kazari4 <skidyuk86115@gmail.com>
2025-08-12 16:54:45 +0900

    オプショナルチェーンを追加

--- src/components/DotPlot/PlotHighlightUser.jsx
+++ src/components/DotPlot/PlotHighlightUser.jsx
@@ -1,20 +1,20 @@
 import DotConnector from "./DotConnector";
 import { createScale } from "../../utils/createScale";
 
-function PlotHighlightUser({ allData, highlightUser, xScale, yScale }) {
+function PlotHighlightUser({ filteredData, highlightUser, xScale, yScale }) {
   // dotPlotのy座標の範囲
   const yRangeMax = 430 - 20;
   const yRange = [yRangeMax, 0];
 
   // 指定ユーザーの座標の配列
-  const highlightUserPositions = allData.map((contestData, i) => {
+  const highlightUserPositions = filteredData.map((contestData, i) => {
     const x = xScale(i);
     const yScale = createScale(contestData.data, "Score", yRange);
     const highlightUserData = contestData.data.find(
       (d) => d.UserScreenName === highlightUser
     );
 
-    const y = yScale(highlightUserData.Score);
+    const y = yScale(highlightUserData?.Score);
     return ({
       x, y
     })
