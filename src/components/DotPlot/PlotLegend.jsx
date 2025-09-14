d99bc3f3e86baeac14da326d77d99e2a7dc48873
kazari4 <skidyuk86115@gmail.com>
2025-09-15 02:03:06 +0900

    ボーダーに関する記述を削除

--- src/components/DotPlot/PlotLegend.jsx
+++ src/components/DotPlot/PlotLegend.jsx
@@ -8,14 +8,6 @@ function PlotLegend({ userName, color }) {
       {/* 赤線：highlightUser */}
       <line x1={0} y1={0} x2={30} y2={0} stroke="rgb(240,0,0)" strokeWidth={2} />
       <text x={35} y={4} fontSize="13">{userName}</text>
-
-      {/* その他の色：指定したPerfのボーダーライン */}
-      {showBorderLine &&
-        <>
-          <line x1={0} y1={spacing} x2={30} y2={spacing} stroke={convertPerformanceColor(color)} strokeWidth={2} strokeDasharray="4 4" />
-          <text x={35} y={spacing + 4} fontSize="13">ボーダーライン</text>
-        </>
-      }
     </>
   );
 }
