35ec04f980280302cae9d12e549167fef13ec8c4
kazari4 <skidyuk86115@gmail.com>
2025-09-15 02:41:56 +0900

    ヘッダーのタイトルを変更

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
     <header className="hero is-info is-small">
       <div className="hero-body">
         <div className="container">
-          <h1 className="title has-text-weight-bold">AHC results visualize</h1>
+          <h1 className="title has-text-weight-bold">AHC results visualizer</h1>
         </div>
       </div>
     </header>
